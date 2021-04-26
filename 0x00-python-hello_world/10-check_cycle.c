1396715abb3af688db7faf46b646451d078330d4
Derek_Clemens <urwithinrange@yahoo.com>
2021-04-26 23:47:20 +0000

    tortice and hare reference

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -12,11 +12,12 @@ int check_cycle(listint_t *list)
 	if (list == NULL)
 		return (0);
 	node2 = list->next;
-	while (node2 != NULL && node2->next != NULL)
+	while (node2 != NULL && node2->next != NULL && node1 != NULL)
 	{
 		if (node1 == node2)
 			return (1);
-		node2 = node2->next;
+		node1 = node1->next;
+		node2 = node2->next->next;
 	}
 	return (0);
 }
