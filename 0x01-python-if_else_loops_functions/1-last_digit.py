88b92b636bfa891a053907340d705ad8e47c1d84
Derek_Clemens <urwithinrange@yahoo.com>
2021-04-27 16:30:46 +0000

    Added space to garer than 5

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -10,7 +10,7 @@ if number < 0:
 else:
     num = number % 10
 if num > 5:
-    print('Last digit of {} is {} and is greater\
+    print('Last digit of {} is {} and is greater \
 than 5'.format(number, num))
 elif num == 0:
     print('Last digit of {} is {} and is 0'.format(number, num))
