0746bf9ea1e097129c9878a6b140422eeb9de450
Derek_Clemens <urwithinrange@yahoo.com>
2021-04-29 04:29:24 +0000

    trying elif

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -3,11 +3,11 @@ def add_tuple(tuple_a=(), tuple_b=()):
     sum1 = 0
     sum2 = 0
     if len(tuple_a) == 0:
-        sum1 = 0 + tuple_b[0]
-        sum2 = 0 + tuple_b[1]
-    if len(tuple_b) == 0:
-        sum1 = 0 + tuple_a[0]
-        sum2 = 0 + tuple_a[1]
+        sum1 += tuple_b[0]
+        sum2 += tuple_b[1]
+    elif len(tuple_b) == 0:
+        sum1 += tuple_a[0]
+        sum2 += tuple_a[1]
     else:
         tempa = list(tuple_a)
         tempb = list(tuple_b)
