e71ead592b782b96132d3dcb6907fc3949c5de7e
Derek_Clemens <urwithinrange@yahoo.com>
2021-04-29 04:49:39 +0000

    corrected comment

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -3,7 +3,7 @@ def add_tuple(tuple_a=(), tuple_b=()):
     sum1 = 0
     sum2 = 0
     if len(tuple_a) < 2:
-        tuple_a += (0, 0) #assigns 0 to an empty space
+        tuple_a += (0, 0)  # assigns 0 to an empty space
     if len(tuple_b) < 2:
         tuple_b += (0, 0)
     sum1 = tuple_a[0] + tuple_b[0]
