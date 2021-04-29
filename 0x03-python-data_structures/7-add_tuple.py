8951c94c4033a07fd4e6a62ab7639b8cacaeaae7
Derek_Clemens <urwithinrange@yahoo.com>
2021-04-29 04:19:54 +0000

    created file and wrote program into it

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    sum1 = 0
+    sum2 = 0
+    if len(tuple_a) == 0:
+        sum1 += tuple_b[0]
+        sum2 += tuple_b[1]
+    if len(tuple_b) == 0:
+        sum1 += tuple_a[0]
+        sum2 += tuple_a[1]
+    else:
+        tempa = list(tuple_a)
+        tempb = list(tuple_b)
+        if len(tempa) == 1:
+            tempa.append(0)
+        if len(tempb) == 1:
+            tempb.append(0)
+        tup_sum1 = tuple(tempa)
+        tup_sum2 = tuple(tempb)
+        sum1 = tup_sum1[0] + tup_sum2[0]
+        sum2 = tup_sum1[1] + tup_sum2[1]
+    new_tuple = (sum1, sum2)
+    return new_tuple
