7fa0d69b6bc442c6c9433a0b03ff6d3a162d0318
Derek_Clemens <urwithinrange@yahoo.com>
2021-04-29 04:47:07 +0000

    Shortened and siplified

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,23 +1,19 @@
-#!/usr/bin/python3
+##usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
     sum1 = 0
     sum2 = 0
-    if len(tuple_a) == 0:
-        sum1 += tuple_b[0]
-        sum2 += tuple_b[1]
-    elif len(tuple_b) == 0:
-        sum1 += tuple_a[0]
-        sum2 += tuple_a[1]
-    else:
-        tempa = list(tuple_a)
-        tempb = list(tuple_b)
-        if len(tempa) == 1:
-            tempa.append(0)
-        if len(tempb) == 1:
-            tempb.append(0)
-        tup_sum1 = tuple(tempa)
-        tup_sum2 = tuple(tempb)
-        sum1 = tup_sum1[0] + tup_sum2[0]
-        sum2 = tup_sum1[1] + tup_sum2[1]
-    new_tuple = (sum1, sum2)
-    return new_tuple
+    if len(tuple_a) < 2:
+        tuple_a += (0, 0) #assigns 0 to an empty space
+    if len(tuple_b) < 2:
+        tuple_b += (0, 0)
+#        tempa = list(tuple_a)
+#        tempb = list(tuple_b)
+# if len(tempa) == 1:
+#           tempa.append(0)
+#       if len(tempb) == 1:
+#           tempb.append(0)
+#        tup_sum1 = tuple(tempa)
+#        tup_sum2 = tuple(tempb)
+    sum1 = tuple_a[0] + tuple_b[0]
+    sum2 = tuple_a[1] + tuple_b[1]
+    return (sum1, sum2)
