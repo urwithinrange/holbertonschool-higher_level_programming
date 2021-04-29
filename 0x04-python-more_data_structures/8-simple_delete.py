d20d0c5bb0b803473d09b45ae79a915397ad47e0
Derek_Clemens <urwithinrange@yahoo.com>
2021-04-29 21:29:16 +0000

    modified the if condition

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def simple_delete(a_dictionary, key=""):
-    new_dict = a_dictionary.copy()
-    if key in new_dict:
-        del new_dict[key]
-    return new_dict
+    if key in a_dictionary:
+        del a_dictionary[key]
+    return a_dictionary
