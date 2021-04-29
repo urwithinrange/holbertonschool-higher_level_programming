811ed02f66b8217278d2ec2a34fb5503ef12f77b
Derek_Clemens <urwithinrange@yahoo.com>
2021-04-29 04:48:22 +0000

    fixed shebang:

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,4 +1,4 @@
-##usr/bin/python3
+#!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
     sum1 = 0
     sum2 = 0
@@ -6,14 +6,6 @@ def add_tuple(tuple_a=(), tuple_b=()):
         tuple_a += (0, 0) #assigns 0 to an empty space
     if len(tuple_b) < 2:
         tuple_b += (0, 0)
-#        tempa = list(tuple_a)
-#        tempb = list(tuple_b)
-# if len(tempa) == 1:
-#           tempa.append(0)
-#       if len(tempb) == 1:
-#           tempb.append(0)
-#        tup_sum1 = tuple(tempa)
-#        tup_sum2 = tuple(tempb)
     sum1 = tuple_a[0] + tuple_b[0]
     sum2 = tuple_a[1] + tuple_b[1]
     return (sum1, sum2)
