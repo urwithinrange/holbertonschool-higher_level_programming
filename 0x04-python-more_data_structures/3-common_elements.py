2edd774bc3779eb50dbcaa626df15cd6e5f0120f
Derek_Clemens <urwithinrange@yahoo.com>
2021-04-29 19:58:02 +0000

    changed to use opperand &

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-    temp = set(set_2)
-    match = [i for i, val in enumerate(set_1) if val in temp]
-    return match
+    return (set_1 & set_2)  # returns the elements that are true in both 
