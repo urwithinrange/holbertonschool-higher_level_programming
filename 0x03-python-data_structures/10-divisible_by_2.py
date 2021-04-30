aab64e033752c29418f3c59499a190c2c4c2eb64
Derek_Clemens <urwithinrange@yahoo.com>
2021-04-30 16:00:18 +0000

    Converted to lambda

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,11 +1,5 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    new_list = []
     if my_list is None:
         return None
-    for i in my_list:
-        if my_list[i] % 2 == 0:
-            new_list.append(True)
-        else:
-            new_list.append(False)
-    return new_list
+    return (list(map(lambda x: x % 2 == 0, my_list)))
