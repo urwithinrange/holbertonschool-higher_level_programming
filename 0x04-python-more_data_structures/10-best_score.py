098ef36aec6bfdfa3b32c3a8e6469597659a4614
Derek_Clemens <urwithinrange@yahoo.com>
2021-04-30 00:20:30 +0000

    fixed shebang

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,4 +1,4 @@
-#!/usr/bin.python3
+#!/usr/bin/python3
 def best_score(a_dictionary):
     if a_dictionary is not None and a_dictionary:
         key_value = sorted(a_dictionary, key=(lambda key: a_dictionary[key]))
