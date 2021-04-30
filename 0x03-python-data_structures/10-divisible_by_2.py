e5fab56fdac9ad644c2623bc8e3b1749c1031388
Derek_Clemens <urwithinrange@yahoo.com>
2021-04-30 15:56:30 +0000

    checking if None is handled

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
     new_list = []
+    if my_list is None:
+        return None
     for i in my_list:
         if my_list[i] % 2 == 0:
             new_list.append(True)
