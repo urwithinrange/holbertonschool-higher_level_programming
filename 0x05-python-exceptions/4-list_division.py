f00c137373a594e7e0d4e4969d8468670edf0f2e
Derek_Clemens <urwithinrange@yahoo.com>
2021-05-04 00:41:19 +0000

    created file and wrote program into it

---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,17 @@
+def list_division(my_list_1, my_list_2, list_length):  # whish list is longer
+    try:
+        quotient = []
+        for i in range(0, list_length):
+            try:
+                quotient.append(my_list_1[i] / my_list_2[i])
+            except ZeroDivisionError:
+                print("division by 0")
+                quotient.append(0)
+            except TypeError:
+                print("wrong type")
+                quotient.append(0)
+            except IndexError:
+                print("out of range")
+                quotient.append(0)
+    finally:
+        return quotient
