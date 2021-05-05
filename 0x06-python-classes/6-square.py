c75a2872a637120d7d1093bfb56cb869532cf52a
Derek_Clemens <urwithinrange@yahoo.com>
2021-05-05 01:10:13 +0000

    lengthened documentation

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -6,7 +6,7 @@ class Square:
     """Create a 2d square,Modules used to manipulate a square"""
 
     def __init__(self, size=0, position=(0, 0)):
-        """Size of the square is determined"""
+        """Size of the square is determined                  """
         self.__size = size
         if not isinstance(position, tuple) and len(position) == 2\
                 and isinstance(position[0], int) and position[0] >= 0 and\
@@ -16,17 +16,17 @@ class Square:
             self.__position = position
 
     def area(self):
-        """Identifies the area of square"""
+        """Identifies the area of square                      """
         return self.__size * self.__size
 
     @property
     def size(self):
-        """Getter for size"""
+        """Getter for size                                    """
         return self.__size
 
     @size.setter
     def size(self, value):
-        """Setter for size"""
+        """Setter for size                                    """
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         if value < 0:
@@ -34,7 +34,7 @@ class Square:
         self.__size = value
 
     def my_print(self):
-        """Where all the printing happens"""
+        """Where all the printing happens                     """
         if self.__size == 0:
             print()
             return
@@ -45,12 +45,12 @@ class Square:
 
     @property
     def position(self):
-        """Getter for position"""
+        """Getter for position                                """
         return self.__position
 
     @position.setter
     def position(self, value):
-        """Setter for position"""
+        """Setter for position                                """
         if type(value) is tuple and len(value) == 2 and\
                 value[0] >= 0 and value[1] >= 0 and type(value[0]) is int\
                 and type(value[1]) is int:
