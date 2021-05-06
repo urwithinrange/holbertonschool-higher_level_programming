66c5c734bf639a61b61e04f07d0e94ac9eea670d
Derek_Clemens <urwithinrange@yahoo.com>
2021-05-06 02:24:18 +0000

    Added some features to try

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -7,17 +7,22 @@ a matrix with multiple list of the same size by a divisor
 
 def matrix_divided(matrix, div):
     """Divide all elements of a matrix by div"""
+    if not(matrix or isinstance(matrix, list)):
+        TypeError("matrix must be a matrix (list of lists)"
+                  "of integers/floats")
     if not all(len(sub_list) == len(matrix[0]) for sub_list in matrix):
         raise TypeError("Each row of the matrix must have the same size")
     if not (isinstance(div, int) or isinstance(div, float)):
         raise TypeError("div must be a number")
     for x in matrix:
-        if len(x) == 0 or not matrix:
+        if len(x) == 0:
+            raise TypeError("matrix must be a matrix (list of lists)"
+                            "of integers/floats")
+        if not isinstance(x, list):
             raise TypeError("matrix must be a matrix (list of lists)"
                             "of integers/floats")
         for y in x:
-            if not(isinstance(y, int) or isinstance(y, float) and
-                    isinstance(matrix, list) and isinstance(x, list)):
+            if not(isinstance(y, int) or isinstance(y, float)):
                 raise TypeError("matrix must be a matrix (list of lists)"
                                 "of integers/floats")
     if div == 0:
