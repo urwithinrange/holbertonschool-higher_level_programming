59dd5d2a2abeec4d01937b2e4721c0b895e66415
Derek_Clemens <urwithinrange@yahoo.com>
2021-05-07 05:19:48 +0000

    changed isinstance()

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,13 +1,9 @@
 #!/usr/bin/python3
-"""
-add_integer adds integers
-"""
-
-
+"""add_integer adds integers"""
 def add_integer(a, b=98):
     """add_integer adds a + b"""
-    if not isinstance(a, int) and not isinstance(a, float):
+    if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
-    if not isinstance(b, int) and not isinstance(b, float):
+    if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
     return int(a) + int(b)
