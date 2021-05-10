d1a609697b8174f80878713f47ad7107dd1a6a83
Derek_Clemens <urwithinrange@yahoo.com>
2021-05-10 03:09:37 +0000

    created file and wrote program into it

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+"""
+This Class Rectangle will create a rectangle
+"""
+
+
+class Rectangle:
+    """
+    Rectangle contains all the functions and \
+    modules to create rectangle
+    """
+
+    def __init__(self, width=0, height=0):
+        """Values for the height and width of rectangle"""
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        return self.__height * self.__width
+
+    def perimeter(self):
+        if self.__height == 0 or self.__width == 0:
+            return
+        return (self.__height * 2) + (self.__width * 2)
