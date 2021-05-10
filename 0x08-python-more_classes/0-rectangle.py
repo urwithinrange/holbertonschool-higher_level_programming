a1fbb1024e2153c9c1698663a49b1b8061efc951
Derek_Clemens <urwithinrange@yahoo.com>
2021-05-10 02:38:42 +0000

    created file and wrote program into it

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+This Class Rectangle will create a rectangle
+"""
+
+
+class Rectangle:
+    """
+    Rectangle contains all the functions and \
+    modules to create rectangle
+    """
+    def __init__(self):
+        pass
