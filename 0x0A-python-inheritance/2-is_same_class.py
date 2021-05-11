c07c25cd7be60602d9135c9ccab5745df07c8c74
Derek_Clemens <urwithinrange@yahoo.com>
2021-05-11 18:47:59 +0000

    Trying type instead of isinstance

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -4,7 +4,7 @@
 
 def is_same_class(obj, a_class):
     """Checking a_class to determine its type"""
-    if isinstance(obj, a_class):
+    if type(obj) is a_class:
         return True
     else:
         return False
