9bee103cfac7b681dcbf91880dec882a53b37640
Derek_Clemens <urwithinrange@yahoo.com>
2021-05-12 01:42:05 +0000

    created file and wrote program into it

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Class BaseGeometry"""
+
+
+class BaseGeometry:
+    """BaseGeometry does something using area soon"""
+    def area(self):
+        raise Exception("area() is not implimented")
