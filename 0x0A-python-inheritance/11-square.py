22b129a5ee69aa1e52eb6c6992977adc89af1da2
Derek_Clemens <urwithinrange@yahoo.com>
2021-05-12 04:02:52 +0000

    created file and wrote program into it

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Class BaseGeometry"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Square takes a sigle dimension"""
+    def __init__(self, size):
+        super().integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def area(self):
+        return self.__size ** 2
+
+    def __str__(self):
+        return ("[Square] {}/{}".format(self.__size, self.__size))
