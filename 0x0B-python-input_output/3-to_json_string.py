6622a1eca65d238d994e0564e60a03ece4ce16d0
Derek_Clemens <urwithinrange@yahoo.com>
2021-05-12 20:33:44 +0000

    created file and wrote program into it

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,11 @@
+#!/usr/bun/python3
+import json
+"""to_json_string is a function using JSON"""
+
+
+def to_json_string(my_obj):
+    """
+    to_json_sting is going to return
+    the JSON representation of an object(string)
+    """
+    return (json.dumps(my_obj))
