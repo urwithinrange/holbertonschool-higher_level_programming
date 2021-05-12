8daf55e7cb6096bfec0bf6556b2422eedf52cd64
Derek_Clemens <urwithinrange@yahoo.com>
2021-05-12 03:19:38 +0000

    Added documentation for init

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -6,6 +6,7 @@ class Rectangle(BaseGeometry):
     """Rectangle will utilize class BaseGeometry"""
 
     def __init__(self, width, height):
+        """Values passed in for the size of rectangle"""
         super().__init__()
         self.__width = self.integer_validator("width", width)
         self.__height = self.integer_validator("height", height)
