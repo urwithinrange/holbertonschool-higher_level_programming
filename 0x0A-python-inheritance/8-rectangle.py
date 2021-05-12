9796a867f08425087349db8eae9d0383fcc49729
Derek_Clemens <urwithinrange@yahoo.com>
2021-05-12 03:21:35 +0000

    pep8 check

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-BaseGeometry = __import__('7-base_geometry').BaseGeometry
 """Class Rectangle will take deminsions"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
 
 class Rectangle(BaseGeometry):
     """Rectangle will utilize class BaseGeometry"""
