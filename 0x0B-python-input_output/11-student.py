f0589ff1d1c0f9117b7234d25e4739fd3b89cffb
Derek_Clemens <urwithinrange@yahoo.com>
2021-05-13 03:06:53 +0000

    created file and wrote program into it

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""class Student defines a student"""
+
+
+class Student:
+    """Student will recieve input to define instance attributes"""
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if attrs is None:
+            return dict((key, value) for (key, value) in self.__dict__.items())
+        else:
+            return dict((key, value) for (key, value) in self.__dict__.items()
+                        if key in attrs)
+    def reload_from_json(self, json):
+        for key, value in json.items():
+            self.__dict__[key] = value
