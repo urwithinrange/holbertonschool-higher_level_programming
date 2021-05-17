b0601a912c6b7920785b268d46efe4d9f0d3eeef
Derek_Clemens <urwithinrange@yahoo.com>
2021-05-17 05:42:59 +0000

    pep8

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -60,6 +60,6 @@ class Base:
         if cls.__name__ == "Rectangle":
             dummy = cls(1, 1)
         elif cls.__name__ == "Square":
-            dummy =  cls(1)
+            dummy = cls(1)
         dummy.update(**dictionary)
         return dummy
