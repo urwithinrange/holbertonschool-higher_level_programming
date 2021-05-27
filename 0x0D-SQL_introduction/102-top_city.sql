d7070f07d02624e79779afb81fc68bb395523559
Derek_Clemens <urwithinrange@yahoo.com>
2021-05-27 03:45:45 +0000

    made small change

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,2 +1,2 @@
 -- highest city temp top 3
-SELECT city,AVG(value) AS avg_temp FROM temperatures GROUP BY state city ORDER BY avg_temp DEC LIMIT 3;
+SELECT city,AVG(value) AS avg_temp FROM temperatures GROUP BY city ORDER BY avg_temp DEC LIMIT 3;
