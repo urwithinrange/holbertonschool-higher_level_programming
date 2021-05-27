f9fc8f750cd1df7e0967c75968a1672bba572d03
Derek_Clemens <urwithinrange@yahoo.com>
2021-05-27 03:22:56 +0000

    fixed order by

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,2 +1,2 @@
 -- displays temperature averages
-SELECT city, AVG(value) AS avg_temp FROM temperatures GROUP BY city ORDER BY value DESC;
+SELECT city,AVG(value) AS avg_temp FROM temperatures GROUP BY city ORDER BY avg_temp DESC;
