d2d1829279085cf009c52fe25cf5dcaa50c6402d
Derek_Clemens <urwithinrange@yahoo.com>
2021-05-27 19:39:57 +0000

    fixed syntax

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,5 +1,6 @@
 -- script that creates the database hbtn_0d_2 and the user user_0d_2
 CREATE DATABASE IF NOT EXISTS user_0d_2;
-CREATE USER IF NOT EXIST 'user_0d_2'@localhost;
-IDENTIFIED BY 'user_0d_2_pwd'
-GRANT USAGE ON *.* IF NOT EXITSTS TO 'user_0d_2'@localhost;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost';
+SET PASSWORD FOR 'user_0d_2'@'localhost' = 'user_0d_2_pwd';
+GRANT USAGE ON *.* TO 'user_0d_2'@'localhost';
+GRANT SELECT ON `hbtn_0d_2`.* TO 'user_0d_2'@'localhost';
