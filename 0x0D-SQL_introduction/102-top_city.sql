bab202d0a8287e724dced0ea3ec9d5659b042e0d
Derek_Clemens <urwithinrange@yahoo.com>
2021-05-27 03:49:52 +0000

    Added month

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,6 +1,7 @@
 -- highest city temp top 3
-SELECT city,AVG(value) AS avg_temp
+SELECT city, AVG(value) AS avg_temp
 FROM temperatures
+WHERE month = 7 OR month = 8
 GROUP BY city
 ORDER BY avg_temp DESC
 LIMIT 3;
