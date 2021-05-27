0873d516fdde24d9c1601d890f2612ac68176f7e
Derek_Clemens <urwithinrange@yahoo.com>
2021-05-27 03:43:00 +0000

    Added limit

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,2 +1,2 @@
 -- highest city temp top 3
-SELECT city,AVG(value) AS avg_temp FROM temperatures GROUP BY state ORDER BY avg_temp ASC;
+SELECT city,AVG(value) AS avg_temp FROM temperatures GROUP BY state ORDER BY avg_temp ASC LIMIT 3;
