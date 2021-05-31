a0fe8aca08eb91a68dc9fbf23c9d077414487584
Derek_Clemens <urwithinrange@yahoo.com>
2021-05-31 20:53:54 +0000

    added format to print

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
-'''displays all vaues in the states table'''
+'''
+displays all vaues in the states table
+'''
 
 if __name__ == "__main__":
     from sys import argv
     import MySQLdb
 
-    # .format
     db = MySQLdb.connect("localhost", argv[1], argv[2], argv[3])
     cursor = db.cursor()
     sql = "SELECT * FROM states WHERE name = '"+argv[4]+"'"
     cursor.execute(sql)
     data = cursor.fetchall()
     for list in data:
-        print(list)
+        print("{}".format(list))
 db.close()
