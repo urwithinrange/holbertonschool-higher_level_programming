4cb9254f9d83bf5670d93ed007e8f6b6abc11e3e
Derek_Clemens <urwithinrange@yahoo.com>
2021-05-31 22:05:39 +0000

    Created file and wrote program into it

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+'''
+script that takes in arguments
+and displays all values in the states table
+but it protected from mysql injections
+'''
+if __name__ == "__main__":
+    from sys import argv
+    import MySQLdb
+
+    db = MySQLdb.connect("localhost", argv[1], argv[2], argv[3])
+    cursor = db.cursor()
+    sql = "SELECT cities.id, cities.name, states.name\
+           FROM citiesINNER JOIN states\
+           ON cities.state_id = states.id"
+    cursor.execute(sql)
+    data = cursor.fetchall()
+    for list in data:
+        print("{}".format(list))
