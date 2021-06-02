32e203b9d8ddbb688ec4afb61acfcc3c82bb5003
Derek_Clemens <urwithinrange@yahoo.com>
2021-06-02 19:55:31 +0000

    updated output

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const myVar = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+const myVar = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 console.log(myVar[0]);
 console.log(myVar[1]);
 console.log(myVar[2]);
