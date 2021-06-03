92d55dc37601e27305eb716a24581d96f3ea4f59
Derek_Clemens <urwithinrange@yahoo.com>
2021-06-03 04:00:12 +0000

    Created file and wrote program into it

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const sym = 'X';
+const num = parseInt(process.argv[2]);
+if (isNaN(num)){
+  console.log("Missing size");
+} else {
+    for (let i = 0; i < num; i++) {
+      console.log(sym.repeat(num))
+    }
+}
+
