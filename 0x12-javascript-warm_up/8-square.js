4fdc72f48dc1e9844c7d2d4ba6ceb41414a449c6
Derek_Clemens <urwithinrange@yahoo.com>
2021-06-03 04:20:47 +0000

    std

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,11 +1,10 @@
 #!/usr/bin/node
 const sym = 'X';
 const num = parseInt(process.argv[2]);
-if (isNaN(num)){
-  console.log("Missing size");
+if (isNaN(num)) {
+  console.log('Missing size');
 } else {
-    for (let i = 0; i < num; i++) {
-      console.log(sym.repeat(num))
-    }
+  for (let i = 0; i < num; i++) {
+    console.log(sym.repeat(num));
+  }
 }
-
